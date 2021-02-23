500ea54e324d161109462eb2a3edb1d3a4869e09
Atul Kumar Singh <atul.kumar@codelogicx.com>
2021-02-23 12:38:10 +0530

    images update

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -8,7 +8,7 @@
       Let me tell you a few things...
     </h2>
     <div class="about-info">
-      <img src="/atul7.jpg" alt="John Doe" class="bio-image">
+      <img src="/images/atul7.jpg" alt="John Doe" class="bio-image">
 
       <div class="bio">
         <h3 class="text-secondary">BIO</h3>
